17ababef2b67cfbc5b702757265912d55ad82e25
Shuto Urabe <urabe_s25@yahoo.co.jp>
2024-06-02 19:36:29 +0900

    Routineのコントローラーをテスト

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -6,4 +6,37 @@ class RoutinesController < ApplicationController
   def show
     @routine = Routine.find(params[:id])
   end
+
+  def new
+    @routine = Routine.new()
+  end
+
+  def create
+    @routine = Routine.new(routine_params)
+
+    if @routine.save
+      redirect_to @routine
+    else
+      render :new, status: :unprocessble_entity
+    end
+  end
+
+  def edit
+    @routine = Routine.find(params[:id])
+  end
+
+  def update
+    @routine = Routine.find(params[:id])
+
+    if @routine.update(routine_params)
+      redirect_to @routine
+    else
+      render :new, status: :unprocessble_entity
+    end
+  end
+
+  private
+    def routine_params
+      params.require(:routine).permit(:title, :body)
+    end
 end
